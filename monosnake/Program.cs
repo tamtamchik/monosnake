95c06d16b7d09364e74af7688c9f88e3de7b0125
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2016-09-15 10:46:32 +0300

    Initial programm

---
+++ monosnake/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace monosnake
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+		}
+	}
+}
